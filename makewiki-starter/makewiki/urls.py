d05d3aebc9540fb7de80f551f7b4b8da937365c5
Anthony <sean.a.protho1@gmail.com>
2019-11-19 15:53:13 -0800

    setup fix

---
+++ makewiki-starter/makewiki/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    # Admin Site
+    path('admin/', admin.site.urls),
+
+    # Wiki App
+    path('', include('wiki.urls')),
+]
